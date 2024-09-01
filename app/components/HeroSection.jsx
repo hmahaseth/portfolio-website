36e18448ec565d22810dd3cc71ba9ace43e4be15
hmahaseth <thegr8empires@gmail.com>
2024-09-01 11:13:11 +0545

    btn

--- app/components/HeroSection.jsx
+++ app/components/HeroSection.jsx
@@ -49,7 +49,7 @@ const HeroSection = () => {
             ensuring that every project not only looks great but performs
             seamlessly.
           </p>
-          <div data-aos="fade-up">
+          <div>
             {" "}
             <a
               href="https://www.linkedin.com/in/royal-empire/"
@@ -57,7 +57,10 @@ const HeroSection = () => {
               rel="noopener noreferrer"
               className="inline-block"
             >
-              <button className="px-6 py-3 w-full sm:w-fit rounded-full mr-4 bg-gradient-to-br from-blue-500 via-purple-500 to-pink-500 hover:bg-slate-200 text-white">
+              <button
+                data-aos="fade-up"
+                className="px-6 py-3 w-full sm:w-fit rounded-full mr-4 bg-gradient-to-br from-blue-500 via-purple-500 to-pink-500 hover:bg-slate-200 text-white"
+              >
                 Contact
               </button>
             </a>
@@ -67,7 +70,10 @@ const HeroSection = () => {
               rel="noopener noreferrer"
               className="inline-block"
             >
-              <button className="px-1 py-1 w-full sm:w-fit rounded-full bg-gradient-to-br from-blue-500 via-purple-500 to-pink-500 hover:bg-slate-800 text-white   mt-3">
+              <button
+                data-aos="fade-up"
+                className="px-1 py-1 w-full sm:w-fit rounded-full bg-gradient-to-br from-blue-500 via-purple-500 to-pink-500 hover:bg-slate-800 text-white   mt-3"
+              >
                 <span className="block bg-black hover:bg-slate-800 rounded-full px-5 py-2">
                   Follow on
                 </span>
